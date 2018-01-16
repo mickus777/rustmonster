d030671743c6ba0137f5ddb4546c9a5e3e8bdc08
Mikael Björk <bjorn.mikael.bjork@gmail.com>
2018-01-16 13:17:57 +0100

    Added more unit tests for the csv reader.

--- rustmonster/csvreader.py
+++ rustmonster/csvreader.py
@@ -1,48 +1,81 @@
 
+def isfloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
 class CSVReader:
 
-    def read_lines(self, header, lines, start_index, item, path):
-        values = lines[start_index].split(u',')
+    def read_item(self, header, lines, index, values):
+        if values[1] == u"object":
+            return self.read_object(header, lines, index, dict(), values[0])
+        elif values[1] == u"list":
+            return self.read_list(header, lines, index, values[0])
+        else:
+            return self.read_value(values[2]), index + 1
 
-        # Read plain attributes
-        for column in range(2, len(header)):
-            value = values[column]
+    def read_value(self, text):
+        if len(text) == 0:
+            return None
+        elif text.startswith(u'"'):
+            return text[1:-1]
+        elif text.isnumeric():
+            return int(text)
+        elif isfloat(text):
+            return float(text)
+        else:
+            raise ValueError("Unknown value type: '" + text + "'.")
+
+    def read_object(self, header, lines, start_index, item, path):
+        # Read plain values
+        values = lines[start_index].split(u';')
+        for column in range(3, len(header)):
+            if column >= len(values):
+                print values
+            value = self.read_value(values[column])
             if value is not None:
-                if value.startswith(u'"'):
-                    item[header[column]] = value[1:-1]
-                elif len(value) == 0:
-                    pass
-                else:  # Missing floating points
-                    item[header[column]] = int(value)
+                item[header[column]] = value
 
         # Read sub objects
         index = start_index + 1
         while index < len(lines):
             sub_line = lines[index]
-            sub_values = sub_line.split(u',')
-            if sub_values[0].startswith(path):
+            sub_values = sub_line.split(u';')
+            if sub_values[0] == path:
+                break  # This is an element in a list
+            elif sub_values[0].startswith(path):
                 name = sub_values[0].split(u'.')[-1]
-                if sub_values[1] == u"object":
-                    item[name], index = self.read_lines(header, lines, index, dict(), sub_values[0])
-                else:
-                    index += 1
-                # Missing list object
+                item[name], index = self.read_item(header, lines, index, sub_values)
             else:
                 break
 
         return item, index
 
+    def read_list(self, header, lines, index, path):
+        lst = []
+        index += 1
+
+        while index < len(lines):
+            line = lines[index]
+            values = line.split(u';')
+            if values[0].startswith(path):
+                item, index = self.read_item(header, lines, index, values)
+                lst.append(item)
+            else:
+                break
+
+        return lst, index
 
     def read(self, text):
 
         lines = text.split(u'\n')
 
-        header = lines[0].split(u',')
+        header = lines[0].split(u';')
 
         root = dict()
 
-        self.read_lines(header, lines, 1, root, u"ROOT")
+        self.read_object(header, lines, 1, root, u"ROOT")
 
         return root
-
-
--- tests/test_csvreader.py
+++ tests/test_csvreader.py
@@ -9,33 +9,120 @@ class TestCSVReader(unittest.TestCase):
         pass
 
     def test_simple_csv(self):
-        csv = u"tag_name,tag_type,name,age,city\n" \
-              u"ROOT,object,\"John\",31,\"New York\""
+        csv = u"tag_name;tag_type;tag_value;name;age;city\n" \
+              u"ROOT;object;;\"John\";31;\"New York\""
 
-        expected = {u"name": u"John", u"age": 31, u"city": u"New York"}
+        expected = {u"name": u"John",
+                    u"age": 31,
+                    u"city": u"New York"}
+
+        result = CSVReader().read(csv)
+
+        self.assertEqual(expected, result)
+
+    def test_csv_with_float(self):
+        csv = u"tag_name;tag_type;tag_value;name;height;city\n" \
+              u"ROOT;object;;\"John\";1.87;\"New York\""
+
+        expected = {u"name": u"John",
+                    u"height": 1.87,
+                    u"city": u"New York"}
 
         result = CSVReader().read(csv)
 
         self.assertEqual(expected, result)
 
     def test_csv_with_object(self):
-        csv = u"tag_name,tag_type,name,age,city\n" \
-              u"ROOT,object,\"John\",31,\n" \
-              u"ROOT.address,object,,,\"New York\""
+        csv = u"tag_name;tag_type;tag_value;name;age;city\n" \
+              u"ROOT;object;;\"John\";31;\n" \
+              u"ROOT.address;object;;;;\"New York\""
 
-        expected = {u"name": u"John", u"age": 31, u"address": {u"city": u"New York"}}
+        expected = {u"name": u"John",
+                    u"age": 31,
+                    u"address": {u"city": u"New York"}}
 
         result = CSVReader().read(csv)
 
         self.assertEqual(expected, result)
 
     def test_csv_with_multiple_object(self):
-        csv = u"tag_name,tag_type,name,age,city\n" \
-              u"ROOT,object,,,\n" \
-              u"ROOT.person,object,\"John\",31,\n" \
-              u"ROOT.address,object,,,\"New York\""
+        csv = u"tag_name;tag_type;tag_value;name;age;city\n" \
+              u"ROOT;object;;;;\n" \
+              u"ROOT.person;object;;\"John\";31;\n" \
+              u"ROOT.address;object;;;;\"New York\""
 
-        expected = {u"person": {u"name": "John", u"age": 31}, u"address": {u"city": u"New York"}}
+        expected = {u"person": {u"name": u"John",
+                                u"age": 31},
+                    u"address": {u"city": u"New York"}}
+
+        result = CSVReader().read(csv)
+
+        self.assertEqual(expected, result)
+
+    def test_csv_with_list(self):
+        csv = u"tag_name;tag_type;tag_value;name;age;city\n" \
+              u"ROOT;object;;\"John\";;\n" \
+              u"ROOT.cities;list;;;;\n" \
+              u"ROOT.cities.city;object;;;29;\"Philadelphia\"\n" \
+              u"ROOT.cities.city;object;;;31;\"New York\"\n"
+
+        expected = {u"name": u"John",
+                    u"cities": [{u"age": 29, u"city": u"Philadelphia"},
+                                {u"age": 31, u"city": u"New York"}]}
+
+        result = CSVReader().read(csv)
+
+        self.assertEqual(expected, result)
+
+    def test_csv_with_valuelist(self):
+        csv = u"tag_name;tag_type;tag_value;name;age\n" \
+              u"ROOT;object;;\"John\";31\n" \
+              u"ROOT.cities;list;;;\n" \
+              u"ROOT.cities.city;value;\"New York\";;\n" \
+              u"ROOT.cities.city;value;\"Philadelphia\";;\n"
+
+        expected = {u"name": u"John",
+                    u"age": 31,
+                    u"cities": [u"New York",
+                                u"Philadelphia"]}
+
+        result = CSVReader().read(csv)
+
+        self.assertEqual(expected, result)
+
+    def test_medium_csv(self):
+        csv = u"tag_name;tag_type;tag_value;id;type;name;ppu\n" \
+              u"ROOT;object;;\"0001\";\"donut\";\"Cake\";0.55\n" \
+              u"ROOT.batters;object;;;;;\n" \
+              u"ROOT.batters.batter;list;;;;;;\n" \
+              u"ROOT.batters.batter.batter;object;;\"1001\";\"Regular\";;\n" \
+              u"ROOT.batters.batter.batter;object;;\"1002\";\"Chocolate\";;\n" \
+              u"ROOT.batters.batter.batter;object;;\"1003\";\"Blueberry\";;\n" \
+              u"ROOT.batters.batter.batter;object;;\"1004\";\"Devil's Food\";;\n" \
+              u"ROOT.topping;list;;;;;\n" \
+              u"ROOT.topping.topping;object;;\"5001\";\"None\";;\n" \
+              u"ROOT.topping.topping;object;;\"5002\";\"Glazed\";;\n" \
+              u"ROOT.topping.topping;object;;\"5005\";\"Sugar\";;\n" \
+              u"ROOT.topping.topping;object;;\"5007\";\"Powdered Sugar\";;\n" \
+              u"ROOT.topping.topping;object;;\"5006\";\"Chocolate with Sprinkles\";;\n" \
+              u"ROOT.topping.topping;object;;\"5003\";\"Chocolate\";;\n" \
+              u"ROOT.topping.topping;object;;\"5004\";\"Maple\";;\n"
+
+        expected = {u"id": u"0001",
+                    u"type": u"donut",
+                    u"name": u"Cake",
+                    u"ppu": 0.55,
+                    u"batters": {u"batter": [{u"id": u"1001", u"type": u"Regular"},
+                                             {u"id": u"1002", u"type": u"Chocolate"},
+                                             {u"id": u"1003", u"type": u"Blueberry"},
+                                             {u"id": u"1004", u"type": u"Devil's Food"}]},
+                    u"topping": [{u"id": u"5001", u"type": u"None"},
+                                 {u"id": u"5002", u"type": u"Glazed"},
+                                 {u"id": u"5005", u"type": u"Sugar"},
+                                 {u"id": u"5007", u"type": u"Powdered Sugar"},
+                                 {u"id": u"5006", u"type": u"Chocolate with Sprinkles"},
+                                 {u"id": u"5003", u"type": u"Chocolate"},
+                                 {u"id": u"5004", u"type": u"Maple"}]}
 
         result = CSVReader().read(csv)
 
